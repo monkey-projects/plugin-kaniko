e428218e74b489b5a8d08dddb3e6154363384144
Wout Neirynck <wout@monkey-projects.be>
2024-08-13 08:52:32 +0200

    Add github deploy plugin

--- .monkeyci/build.clj
+++ .monkeyci/build.clj
@@ -1,4 +1,7 @@
 (ns plugin-clj.build
-  (:require [monkey.ci.plugin.clj :as p]))
+  (:require [monkey.ci.plugin
+             [clj :as p]
+             [github :as gh]]))
 
-(p/deps-library)
+[(p/deps-library)
+ (gh/release-job)]
