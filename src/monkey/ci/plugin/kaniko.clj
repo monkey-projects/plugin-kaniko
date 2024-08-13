54a088100fdf0c855e8015093b246a6686bf916f
Wout Neirynck <wout@monkey-projects.be>
2024-08-13 16:23:48 +0200

    Add multi-platform-image

--- src/monkey/ci/plugin/kaniko.clj
+++ src/monkey/ci/plugin/kaniko.clj
@@ -1,16 +1,18 @@
 (ns monkey.ci.plugin.kaniko
-  (:require [monkey.ci.build
+  (:require [clojure.string :as cs]
+            [monkey.ci.build
              [api :as api]
              [core :as bc]
              [shell :as s]]))
 
-(def kaniko-version "1.21.0")
+(def kaniko-version "1.23.2")
 
 (defn image
   "Creates a build job that builds and pushes a container image using kaniko."
-  [{:keys [creds-param subdir dockerfile target-img]
+  [{:keys [creds-param subdir dockerfile target-img arch]
     :or {creds-param "dockerhub-creds"
-         dockerfile "Dockerfile"}}
+         dockerfile "Dockerfile"}
+    :as conf}
    ctx]
   (let [wd (cond-> (s/container-work-dir ctx)
              subdir (str "/" subdir))
@@ -18,12 +20,63 @@
         config-dir "/kaniko/.docker"
         config-file (str config-dir "/config.json")]
     (bc/container-job
-     "image"
-     {:image (str "docker.io/monkeyci/kaniko:" kaniko-version)
-      :container/env {"DOCKER_CREDS" creds
-                      ;; Must point to the directory where 'config.json' is in
-                      "DOCKER_CONFIG" config-dir}
-      ;; Kaniko requires that docker credentials are written to file
-      :script [(str "echo $DOCKER_CREDS > " config-file)
-               (format "/kaniko/executor --destination %s --dockerfile %s --context dir://%s"
-                       target-img (str wd "/" dockerfile) wd)]})))
+     (cond-> "image"
+       arch (str "-" (name arch)))
+     (-> {:image (str "docker.io/monkeyci/kaniko:" kaniko-version)
+          :container/env {"DOCKER_CREDS" creds
+                          ;; Must point to the directory where 'config.json' is in
+                          "DOCKER_CONFIG" config-dir}
+          ;; Kaniko requires that docker credentials are written to file
+          :script [(str "echo $DOCKER_CREDS > " config-file)
+                   (format "/kaniko/executor --destination %s --dockerfile %s --context dir://%s"
+                           target-img (str wd "/" dockerfile) wd)]}
+         (merge (select-keys conf [:arch]))))))
+
+(defn image-job
+  "Returns a job fn that pushes an image using given config"
+  [conf]
+  (partial image conf))
+
+(defn manifest
+  "Uses manifest-tool to merge the images built for several architectures into one
+   manifest and pushes it."
+  [{:keys [creds-param archs img-template target-img]
+    :or {creds-param "dockerhub-creds"}
+    :as conf}
+   ctx]
+  (let [creds-path "/tmp/docker-config.json"
+        creds (get (api/build-params ctx) creds-param)]
+    (bc/container-job
+     "push-manifest"
+     ;; TODO Switch to mplatform/manifest-tool as soon as MonkeyCI allows shell-less containers
+     {:image "docker.io/monkeyci/manifest-tool:2.1.7"
+      :container/env {"DOCKER_CREDS" creds}
+      :script [(str "echo $DOCKER_CREDS > " creds-path)
+               (format "/manifest-tool --docker-cfg=%s push from-args --platforms %s --template %s --target %s"
+                       creds-path
+                       (->> archs
+                            (map (comp (partial format "linux/%s64") name))
+                            (cs/join ","))
+                       img-template
+                       target-img)]
+      :dependencies (mapv (comp (partial str "image-") name) archs)})))
+
+(defn manifest-job [conf]
+  (partial manifest conf))
+
+(defn multi-platform-image
+  "Creates jobs for building and pushing a multi-platform-image for the configured
+   architectures and image tag."
+  [{:keys [target-img archs] :as conf} ctx]
+  (letfn [(img-template [arch]
+            (str target-img "-" (name arch)))]
+    (-> (map #(image (-> conf
+                         (dissoc :archs)
+                         (assoc :arch %
+                                :target-img (img-template %)))
+                     ctx)
+             archs)
+        (concat [(manifest (-> conf
+                               (select-keys [:creds-param :archs :target-img])
+                               (assoc :img-template (str target-img "-ARCH")))
+                           ctx)]))))
--- test/monkey/ci/plugin/kaniko_test.clj
+++ test/monkey/ci/plugin/kaniko_test.clj
@@ -5,9 +5,11 @@
              [core :as bc]]
             [monkey.ci.plugin.kaniko :as sut]))
 
+(def test-ctx {})
+
 (deftest image
   (with-redefs [api/build-params (constantly {"dockerhub-creds" "test-creds"})]
-    (let [job (sut/image {} {})]
+    (let [job (sut/image {} test-ctx)]
       (testing "returns container job"
         (is (bc/container-job? job)))
 
@@ -18,4 +20,72 @@
         (is (some? (get-in job [:container/env "DOCKER_CONFIG"]))))
 
       (testing "fetches dockerhub-creds from params"
-        (is (= "test-creds" (get-in job [:container/env "DOCKER_CREDS"])))))))
+        (is (= "test-creds" (get-in job [:container/env "DOCKER_CREDS"])))))
+
+    (testing "can specify architecture"
+      (is (= ::test-arch (:arch (sut/image {:arch ::test-arch} test-ctx)))))
+
+    (testing "includes architecture in job name"
+      (is (= "image-arm" (bc/job-id (sut/image {:arch :arm} test-ctx)))))))
+
+(deftest image-job
+  (testing "returns image fn"
+    (is (fn? (sut/image-job {})))))
+
+(deftest manifest
+  (with-redefs [api/build-params (constantly {"dockerhub-creds" "test-creds"})]
+    (let [job (sut/manifest {:archs [:arm :amd]
+                             :img-template "src-image-ARCH"
+                             :target-img "dest-image"}
+                            test-ctx)]
+      (testing "returns container job"
+        (is (bc/container-job? job)))
+
+      (testing "uses manifest-tool image"
+        (is (re-matches #".*manifest-tool.*" (:image job))))
+
+      (testing "fetches dockerhub-creds from params"
+        (is (= "test-creds" (get-in job [:container/env "DOCKER_CREDS"]))))
+
+      (testing "invokes manifest tool with push args"
+        (is (= "/manifest-tool --docker-cfg=/tmp/docker-config.json push from-args --platforms linux/arm64,linux/amd64 --template src-image-ARCH --target dest-image"
+               (last (:script job)))))
+
+      (testing "is dependent on image jobs"
+        (is (= ["image-arm" "image-amd"]
+               (:dependencies job)))))))
+
+(deftest manifest-job
+  (testing "returns manifest fn"
+    (is (fn? (sut/manifest-job {})))))
+
+(deftest multi-platform-image
+  (with-redefs [api/build-params (constantly {"dockerhub-creds" "test-creds"})]
+    (let [jobs (sut/multi-platform-image
+                {:archs [:arm :amd]
+                 :target-img "test/image:tag"}
+                test-ctx)
+          jobs-by-id (group-by bc/job-id jobs)
+          match-cmdline (fn [job pattern]
+                          (->> (get jobs-by-id job)
+                               first
+                               :script
+                               last
+                               (re-matches pattern)
+                               rest))]
+      (testing "creates image job for each platform"
+        (is (= ["image-arm" "image-amd"]
+               (->> jobs-by-id
+                    (keys)
+                    (filter (partial re-matches #"image-.*"))))))
+
+      (testing "uses architecture in target image name"
+        (is (= "test/image:tag-arm"
+               (first (match-cmdline "image-arm" #".*--destination (\S+).*")))))
+
+      (testing "creates manifest job"
+        (is (contains? jobs-by-id "push-manifest")))
+
+      (testing "pushes manifest to target job"
+        (is (= "test/image:tag"
+               (first (match-cmdline "push-manifest" #".*--target (\S+).*"))))))))
